afde6b8808b5fd5ca8e9b7f8613a9042127e746a
Isac Lee <ilee29@jhmi.edu>
2018-06-11 15:50:50 -0400

    update sv

---
+++ qc/ontQC.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python
+
+import math
+import sys
+import csv
+import argparse
+from collections import namedtuple
+import numpy as np
+
+def parseArgs() :
+    parser = argparse.ArgumentParser( description='QC run from summary file')
+    parser.add_argument('input', nargs='?',type=argparse.FileType('r'),
+            default=sys.stdin,help="input basecall summary file")
+    parser.add_argument('-l','--length',metavar='N',type=int,required=False,
+            default=500,help='filter reads that are shorter than N')
+    args = parser.parse_args()
+    return args
+
+def getN50(lengths_list) :
+    lengths_sorted=np.sort(lengths_list)
+    lengths_cumsum=np.cumsum(lengths_sorted)
+    n50_index=np.argmax(lengths_cumsum>max(lengths_cumsum)/2)
+    n50_length=lengths_sorted[n50_index]
+    return n50_length
+
+QCnumbers = namedtuple('QCnumbers', ['read_num','total_bp','N50_length','Q1_length','mean_length','Q3_length','max_length'])
+class ONTSummary :
+    def __init__(self,cutoff_length=200) :
+        self.thr=cutoff_length
+        self.read_lengths = []
+        self.mean_qscores = []
+        self.read_num=0
+    def update(self,fields) :
+        rlen=int(fields[12])
+        if rlen < self.thr :
+            return
+        self.read_num+=1
+        self.read_lengths.append(rlen)
+        self.mean_qscores.append(float(fields[13]))
+    def getnumbers(self) :
+        mean_length=int(np.mean(self.read_lengths))
+        total=sum(self.read_lengths)
+        q1=int(np.percentile(self.read_lengths,25))
+        q3=int(np.percentile(self.read_lengths,75))
+        max_length=max(self.read_lengths)
+        n50=getN50(self.read_lengths)
+        qcnumbers=QCnumbers(self.read_num,total,
+                n50,q1,mean_length,q3,max_length)
+        return qcnumbers
+
+def ontQC(in_fh,cutoff_length=200) : 
+    summary=ONTSummary(cutoff_length)
+    for line in in_fh :
+        fields = line.strip().split('\t')
+        # remove header
+        if fields[0] == "filename" :
+            continue
+        summary.update(fields)
+    qcnumbers=summary.getnumbers()
+    print('\t'.join([str(x) for x in qcnumbers]))
+
+
+if __name__=="__main__":
+    args=parseArgs()
+    ontQC(args.input,args.length)
---
+++ sv/cnv.R
@@ -0,0 +1,156 @@
+#!/usr/bin/Rscript
+# plot average curves
+library(optparse)
+library(tidyverse)
+library(cluster)
+library(DNAcopy)
+library(parallel)
+
+parseArgs = function(argsin){
+    arglist=list(
+        make_option(c("-o","--output"),type="character",default=NULL,
+                    help="output pdf file path",metavar="/path/to/out"),
+        make_option(c("-w","--window"),type="numeric",default=50000,
+                    help="binning window - should match binned cov window"),
+        make_option(c("-f","--faidx"),type="character",default=NULL,
+                    help="faidx for genomewide plot",metavar="/path/to/faidx")
+    )
+    argparser=OptionParser(option_list=arglist)
+    args=parse_args(argparser,args=argsin,positional_arguments=c(1,100))
+    ##Check if there are any variables, and if not, show help
+    if (is.null(args$options$output)){
+        print_help(argparser)
+        print(args)
+        stop("output must be provided",call.=FALSE)
+    }
+    args
+}
+
+readcounts <- function(fpath){
+    cnames=c("chrom","start","end","count","perbp")
+    dat=read_tsv(fpath,col_names=cnames)
+    dat
+}
+trimCov <- function(dat,faidx,binwin){
+    keepchrom=faidx$chrom[1:40]
+    dat.filt=dat[which(dat$chrom %in% keepchrom),]
+    dat.filt = dat.filt %>% ungroup()%>%
+        mutate(start=floor(start/(10*binwin))*10*binwin) %>%
+        group_by(chrom,start,lab)%>%
+        summarize(count=sum(count))
+    labs=unique(dat$lab)
+    for (lab in labs){
+        medcnt=median(dat.filt$count[which(dat.filt$lab==lab)])
+        dat.filt$count[which(dat.filt$lab == lab &
+                             dat.filt$count > 6 * medcnt )] = 6 * medcnt
+        dat.filt$count[which(dat.filt$lab == lab)] =
+            dat.filt$count[which(dat.filt$lab == lab)]/(medcnt/2)
+    }
+    dat.filt
+}
+
+output="/home/isac/Dropbox/Data/ambic/plots/day_cnv.pdf"
+datpaths=c("/dilithium/Data/Nanopore/projects/choSigmaAgingStudy/analysis/D0/sv/HostD0.counts.10000.txt","/dilithium/Data/Nanopore/projects/choSigmaAgingStudy/analysis/D0/sv/StableGlutD0.counts.10000.txt","/dilithium/Data/Nanopore/projects/choSigmaAgingStudy/analysis/D0/sv/UnstableGlutD0.counts.10000.txt")
+
+binwin=10000
+
+faidxpath="/mithril/Data/NGS/Reference/cho/picr_IgG2/picr_IgG2.fa.fai"
+binwin=10000
+processCNV <- function(datpaths,output,binwin,faidxpath){
+    labels=sapply(strsplit(basename(datpaths),"[.]"),"[[",1)
+    faidx=read_tsv(faidxpath,col_names=c("chrom","len","offset"))
+    faidx$offset=ceiling(faidx$offset/binwin)*binwin
+    dat.list=lapply(seq_along(datpaths),function(i){
+        readcounts(datpaths[i])%>%
+            mutate(lab=labels[i])})
+    dat=do.call(rbind,dat.list)
+    mtidx=grep("MT",dat$chrom)
+    if (length(mtidx) > 0)dat=dat[-grep("MT",dat$chrom),] # remove MT chrom
+    dat.bin=dat%>%
+        mutate(start=floor(start/binwin)*binwin,
+               end=ceiling(end/binwin)*binwin)
+    dat.plt=dat.bin%>%select(-perbp)%>%
+        spread(key=lab,value=count)%>%
+        na.omit()
+    # for genome-wide coverage plot
+    dat.trim.plt=trimCov(dat.bin,faidx,binwin)
+    dat.trim.plt$coordbin=dat.trim.plt$start+
+        faidx$offset[match(dat.trim.plt$chrom,faidx$chrom)]
+    dat.gather=dat.trim.plt %>% ungroup() %>%
+        select(-chrom,-start)
+    faidx.inc=faidx[which(faidx$chrom %in% dat.trim.plt$chrom),]
+    seg.list=list()
+    # segmentation?
+    for ( lab in unique(dat.trim.plt$lab)){
+        dat.sub=dat.trim.plt[which(dat.trim.plt$lab==lab),]
+        cna=CNA(log(dat.sub$count),dat.sub$chrom,dat.sub$start,sampleid=lab)
+        cna.sm=smooth.CNA(cna)
+        cna.seg=segment(cna.sm,min.width=5,verbose=1)
+        seg=as.tibble(segments.summary(cna.seg)[,c("chrom","loc.start","loc.end","seg.mean")])
+        seg$seg.mean=exp(seg$seg.mean)
+        seg$lab=lab
+        seg.list[[lab]]=seg
+    }
+    dat.seg=do.call(rbind,seg.list)
+    dat.seg$start=dat.seg$loc.start+faidx$offset[match(dat.seg$chrom,faidx$chrom)]
+    dat.seg$end=dat.seg$loc.end+faidx$offset[match(dat.seg$chrom,faidx$chrom)]
+    # rectangular plot
+    multbin=1000*binwin
+    dat.rect=dat.gather %>%
+        mutate(bin=floor(coordbin/multbin)*multbin)%>%
+        group_by(lab,bin)%>%
+        summarize(count=mean(count))
+    
+    pdf(output,height=4,width=5,useDingbats=FALSE)
+    combos=t(combn(labels,2))
+    for (i in seq(dim(combos)[1])){
+        onelab=combos[i,1]
+        twolab=combos[i,2]
+        plt.tb=dat.plt%>%
+            select(one=onelab,two=twolab)
+        g=ggplot(plt.tb,aes(x=one,y=two))+theme_bw()+
+            geom_point(size=0.5)+scale_x_log10()+scale_y_log10()+
+            labs(x=onelab,y=twolab)
+        print(g)
+    }
+    for ( i in seq_along(datpaths)){
+        samp=labels[i]
+        plt.tb=dat.gather[which(dat.gather$lab==samp),]
+        g = ggplot(plt.tb,aes(x=coordbin,y=count))+
+            geom_point(size=0.2,alpha=0.2)+
+            geom_vline(xintercept=faidx.inc$offset,
+                       linetype="dashed",size=0.2,alpha=0.5)+
+            theme_bw()+ ggtitle(samp)
+
+        print(g)
+    }
+    g = ggplot(dat.rect,aes(ymin=0,ymax=count,
+                      xmin=bin,xmax=bin+multbin,
+                      fill=lab,group=lab))+
+        geom_rect(alpha=0.4,colour=NA)+
+        geom_vline(xintercept=faidx.inc$offset,
+                   linetype="dashed",size=0.2,alpha=0.5)+
+        theme_bw()+theme(legend.position="bottom")
+    print(g)
+    g = ggplot(dat.seg)+
+        geom_rect(aes(ymin=0,ymax=seg.mean,
+                      xmin=start,xmax=end,fill=lab,group=lab),
+                  alpha=0.6,color=NA)+
+        geom_vline(xintercept=faidx.inc$offset,
+                   linetype="dashed",size=0.2,alpha=0.5)+
+        theme_bw()+theme(legend.position="bottom")
+    print(g)
+    dev.off()
+}
+
+
+
+if (! interactive()){
+    argsin=commandArgs(TRUE)
+    args=parseArgs(argsin)
+    print(args)
+    processCNV(args$args,
+               args$options$output,
+               args$options$window,
+               args$options$faidx)
+}

f316a793bbe0ac7c68eaba6e63c581ed2218998e
Isac Lee <ilee29@jhmi.edu>
2018-11-29 15:53:46 -0500

    atacseq snakemake

--- atac/181129_chosigma_snakemake.py
+++ atac/181129_chosigma_snakemake.py
@@ -3,18 +3,71 @@ import pandas as pd
 from snakemake.utils import validate 
 
 workdir: config['workdir']
+threads: config['threads']
+maxthreads = 36
 samples = pd.read_table(config['samples']).set_index("samples",drop=False)
 
 rule all:
     input: 
-        expand("fastq_trimmed/{sample}_trimmed.fq.gz",sample=samples.index)
-
+        expand("bam/{sample}.nodup.sorted.bam",sample=samples.index)
+        
 rule trim:
-    input :
+    input:
         "fastq/{sample}.fastq.gz"
-    output :
+    output:
         "fastq_trimmed/{sample}_trimmed.fq.gz"
     log:
         "log/trim/{sample}.trim.log"
-    shell :
+    shell:
         "trim_galore {input} -o fastq_trimmed &> {log}"
+
+rule align:
+    input:
+        "fastq_trimmed/{sample}_trimmed.fq.gz"
+    params:
+        btidx=config['bowtieidx']
+    threads: maxthreads # max 36, controlled by "-j" option into command or "threads" config parameter
+    output:
+        temp("bam/{sample}.bam")
+    log:
+        "log/align/{sample}.align.log"
+    shell:
+        "bowtie2 -k 4 -p {threads} -t --local \
+                -x {params.btidx} -U {input} 2> {log} |\
+                samtools view -Sb - > {output}"
+
+rule filter_multimap:
+    input:
+        "bam/{sample}.bam"
+    params:
+        codedir=config['codedir']
+    output:
+        temp("bam/{sample}.multimapfiltered.bam")
+    shell:
+        "samtools sort -n -T bam/{wildcards.sample}.qsorting {input} |\
+                samtools view -h - |\
+                python {params.codedir}/script/assign_multimappers.py -k 4 |\
+                samtools view -F 1804 -Su - |\
+                samtools sort -T bam/{wildcards.sample}.mpsorting -o {output}"
+
+rule remove_duplicates:
+    input:
+        "bam/{sample}.multimapfiltered.bam"
+    threads: maxthreads
+    output:
+        "bam/{sample}.nodup.sorted.bam"
+    log:
+        "log/removedup/{sample}.dupremoval.log"
+    shell:
+        "picard -Xmx8G -Xms256M \
+                -XX:ParallelGCThreads={threads} \
+                -Djava.io.tmpdir=bam/dupremove \
+                MarkDuplicates \
+                INPUT={input} OUTPUT=bam/{wildcards.sample}.dupmark.bam \
+                METRICS_FILE=bam/{wildcards.sample}.duplicate_metrics.txt \
+                VALIDATION_STRINGENCY=LENIENT \
+                ASSUME_SORTED=true REMOVE_DUPLICATES=false \
+                &> {log} &&\
+                samtools view -F 1804 -b bam/{wildcards.sample}.dupmark.bam > {output} &&\
+                samtools index {output}"
+
--- atac/181129_chosigma_snakemake_run.sh
+++ atac/181129_chosigma_snakemake_run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 snake=181129_chosigma_snakemake.py
 config=chosigmaAgingstudy_snake.config
-snakemake -j 8 --snakefile $snake --configfile $config 
+if [ "$1" == "makeflow" ];then
+  snakemake -j 8 --snakefile $snake --configfile $config --dag |\
+    dot -Tsvg > 181129_chosigma_snakemake.dag.svg
+else
+  snakemake -j 8 --snakefile $snake --configfile $config  #-np #--unlock
+fi
+

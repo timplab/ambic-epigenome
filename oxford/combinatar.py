6ee294c2e6fb381ef7bab40feebfa91ed93df6ec
Ubuntu <ubuntu@ip-172-31-68-36.ec2.internal>
2019-01-21 20:08:04 +0000

    reorg

--- oxford/combinatar.py
+++ oxford/combinatar.py
@@ -12,11 +12,18 @@ def parseArgs():
     import argparse
     import os
     parser=argparse.ArgumentParser(description="Combine several nanopore runs into one dir")
-    parser.add_argument('root',metavar='dir',type=os.path.abspath,help='common directory for fast5 files')
-    parser.add_argument('-n',metavar='N',type=int,default=4000,help='number of fast5s to group into one dir')
-    parser.add_argument('-s','--sample',metavar='foo',type=str,required=False,help='name of the run to combine')
-    parser.add_argument('-a','--archive',action='store_true',help='flag for archiving')
-    parser.add_argument('-o','--outdir',metavar='dir',type=os.path.abspath,required=True,help='output directory')
+    parser.add_argument('-i,','--indir',dest='root',metavar='dir',required=True,
+            type=os.path.abspath,help='common directory for fast5 files')
+    parser.add_argument('-o','--outdir',metavar='dir',type=os.path.abspath,
+            required=True,help='output directory')
+    parser.add_argument('-n',metavar='N',type=int,default=4000,
+            help='number of fast5s to group into one dir')
+    parser.add_argument('-s','--sample',metavar='foo',type=str,required=False,
+            help='name of the run to combine')
+    parser.add_argument('-a','--archive',action='store_true',
+            help='flag for archiving')
+    parser.add_argument('--dryrun',action='store_true',
+            help='dry run')
     args=parser.parse_args()
     if args.sample==None:
         ## if sample name is not given, using the dir above root and longest suffix of the dirs as the prefix
@@ -53,25 +60,35 @@ def combineRuns(args):
         folder_ind= 0
     ind=str(folder_ind)
     wdir=os.path.join(outdir,ind)
-    os.mkdir(wdir)
     file_ind = -1
-    totfiles=0
+    totfiles = 0
+    if args.dryrun :
+        print("dryrun",flush=True)
+        def updatecommand(path,newpath) :
+            print("{} to {}".format(path,newpath),flush=True)
+    else :
+        os.mkdir(wdir)
+        def updatecommand(path,newpath) :
+            os.rename(path,newpath)
     for root,dirs,files in os.walk(args.root):
         for f in files:
             if args.sample in f:
                 wdir,file_ind=indUpdate(wdir,file_ind,n)
                 path=os.path.join(root,f)
                 newpath=os.path.join(wdir,f)
-                os.rename(path,newpath)
+                updatecommand(path,newpath)
                 totfiles+=1
     print("total number of files: {}".format(totfiles))
     if args.archive:
         import tarfile
         print("archiving...")
-        with tarfile.open(outdir+".raw.tgz","w:gz") as fh:
-            for i in next(os.walk(outdir))[1]:
-                print(i)
-                fh.add(os.path.join(outdir,i),arcname=i)
+        if args.dryrun :
+            print(outdir+".raw.tgz")
+        else :
+            with tarfile.open(outdir+".raw.tgz","w:gz") as fh:
+                for i in next(os.walk(outdir))[1]:
+                    print(i)
+                    fh.add(os.path.join(outdir,i),arcname=i)
 
 def main():
     args=parseArgs()
